0fedc93bfc2fd74bfde2f92b01ca0e00b74c1715
Haserius <sharpneld@gmail.com>
2024-06-30 23:40:04 -0500

    Recipe changes

--- kubejs/server_scripts/mods/minecraft/recipes.js
+++ kubejs/server_scripts/mods/minecraft/recipes.js
@@ -1,3 +1,48 @@
 ServerEvents.recipes(event => {
     event.remove({ id: 'minecraft:flint_and_steel' })
+    //Add default recipes for smithing upgrade templates
+    event.shaped(
+        Item.of('smithingupgradersplus:iron_smithing_template', 1),
+        [
+            ' D ', 
+            'BAB', 
+            'CCC'
+        ],
+        {
+            A: 'minecraft:stone',
+            B: 'minecraft:iron_block',
+            C: 'minecraft:iron_ingot',
+            D: 'minecraft:flint'
+        }
+    ).id('sharpneld:smithingupgradersplus/recipes/iron_smithing_template')
+
+    event.shaped(
+        Item.of('smithingupgradersplus:golden_smithing_template', 1),
+        [
+            ' D ', 
+            'BAB', 
+            'CCC'
+        ],
+        {
+            A: 'minecraft:stone',
+            B: 'minecraft:gold_block',
+            C: 'minecraft:gold_ingot',
+            D: 'minecraft:flint'
+        }
+    ).id('sharpneld:smithingupgradersplus/recipes/golden_smithing_template')
+
+    event.shaped(
+        Item.of('smithingupgradersplus:diamond_smithing_template', 1),
+        [
+            ' D ', 
+            'BAB', 
+            'CCC'
+        ],
+        {
+            A: 'minecraft:stone',
+            B: 'minecraft:diamond_block',
+            C: 'minecraft:diamond',
+            D: 'minecraft:flint'
+        }
+    ).id('sharpneld:smithingupgradersplus/recipes/diamond_smithing_template')
 })
